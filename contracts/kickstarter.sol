094e96392be29db374acf6834be0953c47dac89d
partylikeits1983 <aleqvids@gmail.com>
2022-01-29 17:04:59 +0300

    readme

--- contracts/kickstarter.sol
+++ contracts/kickstarter.sol
@@ -1,26 +1,5 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.7.0 <0.9.0;
-
-
-/*
-
-    A smart contract that functions as kickstarter: 
-    
-        1) Users can create project and ask for funding
-        2) Users can fund projects they like
-        3) If funding target is met, payment is made available
-
-        ------
-
-        Planned features: 
-        
-        1) 50% of payment is made available when funding target is met, 50% is made available after investors vote to release other 50%
-        2) ERC20 support
-
-*/
-
-
 
 contract kickstarter {
 
